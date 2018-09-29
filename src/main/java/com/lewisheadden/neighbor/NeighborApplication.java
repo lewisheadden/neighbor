eecfd88513e03d0814d0963d3d8d2a45d45d4911
Lewis Headden <lewis@lewisheadden.com>
2018-09-28 21:23:55 -0400

    Initial commit

---
+++ src/main/java/com/lewisheadden/neighbor/NeighborApplication.java
@@ -0,0 +1,12 @@
+package com.lewisheadden.neighbor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeighborApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeighborApplication.class, args);
+	}
+}
